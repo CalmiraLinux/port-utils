376cd527d59a100538cfec24c7e51eecf226551a
Linuxoid85 <michail383krasnov@mail.ru>
2021-08-28 23:36:11 +0300

    Окончательная смена имени утилиты

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-update-ports: src/update-ports.py src/locales/ru.mo src/docs/branches
+update-ports: src/port-utils.py src/locales/ru.mo src/docs/branches
 	echo "You must be run 'make install'!"
 
 install:
@@ -7,6 +7,6 @@ install:
 	msgfmt src/locales/ru.po
 	cp -v src/locales/ru.po   /usr/share/locale/ru/port-utils.mo
 	cp -v src/docs/branches   /usr/share/update-ports/branches
-	cp -v src/update-ports.py /usr/bin/update-ports
+	cp -v src/port-utils.py /usr/bin/port-utils
 
 	chmod +x /usr/bin/update-ports
--- src/port-utils.py
+++ src/port-utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 #
-# update-ports - Port system update program
+# port-utils - Port system software
 # Copyright (C) 2021 Michail Krasnov
 #
 # This program is free software; you can redistribute it and/or modify
@@ -17,7 +17,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
-# Project Page: http://github.com/CalmiraLinux/update-ports
+# Project Page: http://github.com/CalmiraLinux/port-utils
 # Michail Krasnov <linuxoid85@gmail.com>
 #
 
@@ -246,7 +246,7 @@ class Update(object):
         f.write(ufr.content) # Downloading
 
         f.close()
-        
+
         if os.path.isfile(CACHE_FILE):
             print(_("Downloaded successfully!"))
         else:
@@ -279,7 +279,7 @@ class Update(object):
         else:
             print(_("Package unpacking error. Package not found. It may not have been downloaded, or a third-party program changed it's name during unpacking."))
             exit(1)
-    
+
     # Installing the port
     def installPort():
         # Checking just in case
@@ -288,7 +288,7 @@ class Update(object):
         else:
             print(_("Package unpacking error: directory not found."))
             exit(1)
-        
+
         # Checking for a previous version of ports
         if os.path.isdir(PORTDIR):
             print(_("Found directory with previous ports version. Removed..."))
