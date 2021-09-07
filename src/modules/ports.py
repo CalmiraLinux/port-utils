37c513ea44e3544cc5cd4ec29fd98e2ea55d4651
Linuxoid85 <michail383krasnov@mail.ru>
2021-09-07 20:46:05 +0300

    Добавление функции проверки приоритета пакета

--- src/modules/ports.py
+++ src/modules/ports.py
@@ -23,37 +23,90 @@
 
 import os
 import sys
-import argparse
-import shutil
 import tarfile
-import requests
 import gettext
 import json
-from datetime import datetime
-from tkinter import *
-import tkinter as tk
 
 ## BASE CONSTANTS ##
 ports_PORTS = "/usr/ports"
 OK_MSG = "\033[32mOK\033[0m"
 FAIL_MSG = "\033[31mFAIL\033[0m"
 
-# Проверка на существование порта
-def check_port(port):
-    print(_("Checking for the existence of a port {} ...").format(port), end = " ")
 
-    ports_PORTNAME = ports_PORTS + "/" + port
-    ports_PORTJSON = ports_PORTNAME + "/config.json"
+## BASE FUNCTIONS ##
 
-    if os.path.isdir(ports_PORTNAME):
-        if os.path.isfile(ports_PORTJSON):
+##################################
+##                              ##
+## Class for working with files ##
+##                              ##
+##################################
+class files(object):
+    # Checking for the existence of a port file
+    def check_port_file(port_file, mode):
+        if mode == "dir":
+            file_test = os.path.isdir(port_file)
+        elif mode == "file":
+            file_test = os.path.isfile(port_file)
+        else:
+            print(_("'check_port_file': Error: Option {} does not exist!").format(mode))
+            exit(1)
+
+        if file_test:
             print(OK_MSG)
             return 0
         else:
             print(FAIL_MSG)
-
-            print(_("\033[1m\033[31mError: port data file does not exist\033[0m"))
             return 1
-    else:
-        print(_("\033[1m\033[31mError: the directory with the port \033[0m\033[35m'{}'\033[0m\033[1m\033[31m does not exist\033[0m").format(port))
-        return 1
+
+##################################
+##                              ##
+## Class for working with ports ##
+##                              ##
+##################################
+class port(object):
+    # Checking for the existence of a port
+    def check_port(port):
+        print(_("Checking for the existence of a port {} ...").format(port), end = " ")
+
+        ports_PORTNAME = ports_PORTS + "/" + port
+        ports_PORTJSON = ports_PORTNAME + "/config.json"
+
+        if os.path.isdir(ports_PORTNAME):
+            if os.path.isfile(ports_PORTJSON):
+                print(OK_MSG)
+                return 0
+            else:
+                print(FAIL_MSG)
+
+                print(_("\033[1m\033[31mError: port data file does not exist\033[0m"))
+                return 1
+        else:
+            print(_("\033[1m\033[31mError: the directory with the port \033[0m\033[35m'{}'\033[0m\033[1m\033[31m does not exist\033[0m").format(port))
+            return 1
+
+    # Function for check priority of package
+    # If priority = system, then package doesn't
+    # can remove from Calmira GNU/Linux
+    ## Priority:
+    # 'system' and 'user'
+    def check_priority(port):
+        print(_("Port {} priority check...").format(port), end = " ")
+
+        ports_PORTNAME = ports_PORTS + "/" + port
+        ports_PORTJSON = ports_PORTNAME + "/config.json"
+
+        if check_port_file(ports_PORTJSON):
+            with open(ports_PORTJSON, 'r') as f:
+                package_data = json.load(f.read())
+
+                if package_data["priority"] == "system":
+                    return 1
+                elif package_data["priority"] == "user":
+                    return 0
+                else:
+                    print(_("'check_priority': Error: priority {} does not exist!").format(package_data["priority"]))
+                    exit(1)
+
+        else:
+            exit(1)
+
